7de082c8ee0b245039350beb9135fba55d3e17d3
LoganPletcher <lpletch95@hotmail.com>
2016-04-19 20:04:44 -0500

    created a current node and an objective node to work with to figure out how to populate the openlist for A*

--- Balls/PyGameTest2.py
+++ Balls/PyGameTest2.py
@@ -17,20 +17,36 @@ darkBlue = (0,0,128)
 white = (255,255,255)
 black = (0,0,0)
 pink = (255,200,200)
+yellow = (255,255,0)
+purple = (255,0,255)
 screen.fill(green)
-nodes = Counter()
-for i in range(0,10):
-    for j in range(0,10):
-        node = Nodes.Node(i,j)
-        nodes[node] = (i * 10) + j
+nodes = []
+for y in range(0,10):
+    noderow = []
+    for x in range(0,10):
+        node = Nodes.Node(y,x)
+        noderow.append(node)
+    nodes.append(noderow)
 
-for node in nodes:
-    node.draw(screen,green)
+cnode = nodes[2][2]
+onode = nodes[6][2]
+
+for i in range(0,3):
+    nodes[4][i+1].setWalk(False)
+
+openList = [] #Nodes that the current can and could walk to?
 
 while True:
         for event in pygame.event.get():
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
-        #pygame.draw.rect(screen,(255,0,0),(0,0,30,30))
+        print (cnode.pos)
+        for noderow in nodes:
+            for node in noderow:
+                node.draw(screen,green)
+        pygame.draw.rect(screen, purple,
+                         (onode.left, onode.top, onode.width, onode.height))
+        pygame.draw.rect(screen, yellow,
+                         (cnode.left, cnode.top, cnode.width, cnode.height))
         pygame.display.update()
